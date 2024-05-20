2c033e432bda8ebb24c4f03fea325d3864a34ed4
derrekdo <derrekdo123@gmail.com>
2024-05-19 23:37:22 -0700

    move to targeted unit, and action menu(WIP)

--- GameBoard/GameBoard.gd
+++ GameBoard/GameBoard.gd
@@ -2,6 +2,8 @@ class_name GameBoard
 extends Node2D
 
 const DIRECTIONS = [Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN]
+const _unit := "res://Units/Unit.tscn"
+const _action_menu_scene := preload("res://ActionMenu.tscn")
 
 export var grid: Resource
 
@@ -10,13 +12,24 @@ var _units := {}
 var _active_unit: Unit
 var _walkable_cells := []
 
+#prepare/wait for child nodes
 onready var _unit_overlay: UnitOverlay = $UnitOverlay
 onready var _unit_path: UnitPath = $UnitPath
+onready var _action_menu = _action_menu_scene.instance()
 
 
 func _ready() -> void:
 	_reinitialize()
+	add_child(_action_menu)
+	_action_menu.hide()
+	_action_menu.connect("attack_selected", self, "_on_ActionMenu_attack_selected")
+	_action_menu.connect("inventory_selected", self, "_on_ActionMenu_inventory_selected")
+	_action_menu.connect("recruit_selected", self, "_on_ActionMenu_recruit_selected")
+	_action_menu.connect("wait_selected", self, "_on_ActionMenu_wait_selected")
 
+#spawn player chars, and enemy reinforcements?
+func _spawn_units() -> void:
+	pass
 #deselect unit
 func _unhandled_input(event: InputEvent) -> void:
 	if _active_unit and event.is_action_pressed("ui_cancel"):
@@ -83,7 +96,7 @@ func _flood_fill(cell: Vector2, max_distance: int) -> Array:
 	return arr
 
 #Updates the _units dictionary with the target position for the unit and moves it
-func _move_active_unit(new_cell: Vector2) -> void:
+func _move_unit(new_cell: Vector2) -> void:
 	#check if empty cell
 	if is_occupied(new_cell) or not new_cell in _walkable_cells:
 		return
@@ -93,6 +106,7 @@ func _move_active_unit(new_cell: Vector2) -> void:
 	_active_unit.walk_along(_unit_path.curr_path)
 	yield(_active_unit, "walk_finished")
 	_clear_active_unit()
+	_show_action_menu(new_cell)
 
 #Selectst eh unit
 func _select_unit(cell: Vector2) -> void:
@@ -121,9 +135,58 @@ func _on_Cursor_accept_pressed(cell: Vector2) -> void:
 	if not _active_unit:
 		_select_unit(cell)
 	elif _active_unit.is_selected:
-		_move_active_unit(cell)
+		_move_unit(closest_cell(cell))
 
 #updates the path
 func _on_Cursor_moved(new_cell: Vector2) -> void:
 	if _active_unit and _active_unit.is_selected:
-		_unit_path.draw(_active_unit.cell, new_cell)
+		var target_cell = closest_cell(new_cell)
+		_unit_path.draw(_active_unit.cell, target_cell)
+
+#returns selected cell, or closest cell to unit if selecting another unit
+func closest_cell(cell: Vector2) -> Vector2:
+	if not _units.has(cell):
+		return cell
+		
+	var close_cell := cell
+	var min_dist := 100000
+	
+	for direction in DIRECTIONS:
+		var target: Vector2 = cell + direction
+		if _units.has(target) and not _units[target] == _active_unit:
+			continue
+		
+		var dist := target.distance_squared_to(_active_unit.cell)
+		if dist < min_dist:
+			min_dist = dist
+			close_cell = target
+			
+	return close_cell
+
+
+func _on_ActionMenu_attack_selected() -> void:
+	print("attack")
+	pass # Replace with function body.
+
+
+func _on_ActionMenu_inventory_selected() -> void:
+	print("inv")
+	pass # Replace with function body.
+
+
+func _on_ActionMenu_recruit_selected() -> void:
+	print("recruit")
+	pass # Replace with function body.
+
+
+func _on_ActionMenu_wait_selected() -> void:
+	print("wait")
+	pass # Replace with function body.
+
+func _show_action_menu(cell:Vector2) -> void:
+	var screen_pos = cell_to_screen_pos(cell)
+	_action_menu.show_menu(screen_pos)
+
+func cell_to_screen_pos(cell: Vector2) -> Vector2:
+	var cell_size = Vector2(80,80)
+	return cell * cell_size
